a6d88e806d1a16a14e2634ceb1bc61a317886b75
Clyde Baron Rapinan <clydebaron2000@gmail.com>
2020-09-11 17:30:26 -0700

    shortened/ optimized code

--- Develop/app.js
+++ Develop/app.js
@@ -10,16 +10,12 @@ const Questions = require("./lib/Questions.js");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const util = require("util");
-const writeFileAsync = util.promisify(fs.writeFile);
 const chalk = require("chalk");
 //custom functions to enable for loop
 function objContentToList(obj) {
-    let out = [];
-    Object.keys(obj).forEach(key => out.push(obj[key]));
-    return out;
+    return Object.keys(obj).map(key => obj[key]);
 }
-
+//for the loop
 function stringToConstructor(string) {
     if (string === 'manager') return (...input) => new Manager(...input);
     if (string === 'engineer') return (...input) => new Engineer(...input);
@@ -28,26 +24,22 @@ function stringToConstructor(string) {
 // run this block immediately, under async
 (async() => {
     try {
-        let employeeArray = [];
-        //user must input manager
-        let employeeTypes = ['manager'];
+        let employeeArray = [],
+            employeeTypes = ['manager'];
         //for everything in the list
         for (const employeeType of employeeTypes) {
             //prompt
             const employeeData = await inquirer.prompt(Questions[employeeType]);
-            //make object
-            const employeeObj = stringToConstructor(employeeType)(...objContentToList(employeeData));
-            //store
-            employeeArray.push(employeeObj);
+            //make object & store
+            employeeArray.push(stringToConstructor(employeeType)(...objContentToList(employeeData)));
             //prompt add more? can only add engineer and intern
-            const line = new inquirer.Separator('--------');
             const { response } = await inquirer.prompt({
                 type: "list",
                 name: "response",
                 message: chalk `{white Would you like to add another employee?}`,
-                choices: ['engineer', 'intern', line, 'no']
+                choices: ['engineer', 'intern', new inquirer.Separator('--------'), 'no']
             });
-            //if 'no' stop the loop
+            //if 'no' stop the loop, break to ensure loop is exited.
             if (response === 'no') break;
             //else add one more thing to do 
             else employeeTypes.push(response);
@@ -55,10 +47,9 @@ function stringToConstructor(string) {
         //check if file exists, make it if it doesn't
         if (!fs.existsSync("./output")) fs.mkdirSync("./output");
         //get output path
-        const OUTPUT_DIR = path.resolve(__dirname, "output");
-        const outputPath = path.join(OUTPUT_DIR, "team.html");
+        const outputPath = path.join(path.resolve(__dirname, "output"), "team.html");
         //render the html and write it to output dir        
-        writeFileAsync(outputPath, render(employeeArray));
+        fs.writeFileSync(outputPath, render(employeeArray));
         console.log(chalk `{green.bold Success! Your file has been rendered in the output directory as 'team.html'}`);
     } catch (err) { console.log(err) };
 })();
